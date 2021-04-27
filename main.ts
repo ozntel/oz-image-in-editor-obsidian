d1008e41760a64455cd7f99a041740ba5363d4d2
Ozan Tellioglu <ozan@Ozans-MacBook-Pro.local>
2021-04-27 21:29:16 +0200

    Updates

--- main.ts
+++ main.ts
@@ -1,35 +1,165 @@
-import { Plugin, Notice, debounce } from 'obsidian';
+import { Plugin, Notice, MarkdownView } from 'obsidian';
 
 export default class OzanImagePlugin extends Plugin{
 
-    async onload(){
+    onload(){
         // Register Code Mirror
         this.registerCodeMirror( (cm: CodeMirror.Editor) => {
 
             // Check Lines during initial Load
             this.check_lines(cm);
-            
             // Check Lines after each change
-            cm.on("change", debounce(() => this.check_lines(cm), 500, false))
+            cm.on("changes", this.codemirrorLineChanges);
+            // Check Lines once CodeMirror is fired
+            cm.on("focus", this.codemirrorScreenChange);
 
         })
     }
 
-    onunload(){
-        new Notice('Ozan\'s Image in Editor Plugin  is unloaded');
+    codemirrorLineChanges = (cm: any, changes: any) => {
+
+        changes.some( (change: any) => {
+            this.check_line(cm, change.to.line);
+        })
+
     }
 
-    check_lines: any = (cm: CodeMirror.Editor) => {
-        // Attachment Path
-        let files = this.app.vault.getFiles();
+    codemirrorScreenChange = (cm: CodeMirror.Editor) => {
+
+        return this.check_lines(cm);
+
+    }
+
+    onunload(){
+
+        this.app.workspace.iterateCodeMirrors( (cm) => {
+            cm.off("changes", this.codemirrorLineChanges); // IT DOESN'T WORK
+            cm.off("focus", this.codemirrorScreenChange);
+            this.clearWidges(cm);
+        });
+
+        new Notice('Image in Editor Plugin is unloaded');
+
+    }
+
+    // Check Single Line
+    check_line: any = (cm: CodeMirror.Editor, line_number: number) => {
 
         // Regex for [[ ]] format
-        const image_line_regex_1 = /!\[\[.*(jpe?g|png|gif)\]\]/
-    
+        const image_line_regex_1 = /!\[\[.*(jpe?g|png|gif).*\]\]/
+            
         // Regex for ![ ]( ) format
         const image_line_regex_2 = /!\[(^$|.*)\]\(.*(jpe?g|png|gif)\)/
-    
+
+        // Get the Line edited
+        const line = cm.lineInfo(line_number);
+        
+        if(line === null) return;
+
+        // Current Line Comparison with Regex
+        const match_1 = line.text.match(image_line_regex_1);
+        const match_2 = line.text.match(image_line_regex_2);
+
+        // Clear the widget if link was removed
+        var line_image_widget = line.widgets ? line.widgets.filter((wid: { className: string; }) => wid.className === 'oz-image-widget') : false;
+        if(line_image_widget && (!match_1 || !match_2)) line_image_widget[0].clear();
+
+        // If any of regex matches, it will add image widget
+        if(match_1 || match_2){
+             
+            // Clear the image widgets if exists
+            if (line.widgets){
+                for(const wid of line.widgets){
+                    if (wid.className === 'oz-image-widget'){
+                        wid.clear()
+                    }
+                }
+            }
+
+            // Get the file name and alt text depending on format
+            var filename = '';
+            var alt = '';
+            
+            if(match_1){
+                // Regex for [[myimage.jpg|#x-small]] format
+                filename = this.getFileNameAndAltText(1, match_1).fileName
+                alt = this.getFileNameAndAltText(1, match_1).altText
+            } else if(match_2){
+                // Regex for ![#x-small](myimage.jpg) format
+                filename = this.getFileNameAndAltText(2, match_2).fileName
+                alt = this.getFileNameAndAltText(2, match_2).altText
+            }
+            
+            // Create Image
+            const img = document.createElement('img');
+
+            // Prepare the src for the Image
+            if(this.filename_is_a_link(filename)){
+                img.src = filename;
+            } else {
+                var image = this.app.metadataCache.getFirstLinkpathDest(decodeURIComponent(filename), this.getActiveNoteFile().path);
+                if(image != null) img.src =  this.app.vault.getResourcePath(image); 
+                // NOTE: doesn't blink with : 'app://local/Users/ozan/Desktop/Debug/' + image.path
+            }
+
+            // Image Properties
+            img.alt = alt;
+            img.style.maxWidth = '100%';
+            img.style.height = 'auto';
+            
+            // Add Image widget under the Image Markdown
+            cm.addLineWidget(line_number, img, {className: 'oz-image-widget'});            
+        }
+    }
+
+    // Check All Lines Function
+    check_lines: any = (cm: CodeMirror.Editor) => {
+
         // Last Used Line Number in Code Mirror
+        var lastLine = cm.lastLine();
+
+        for(let i=0; i <= lastLine; i++){
+            this.check_line(cm, i);
+        }        
+
+    }
+
+    // Http, Https Link Check
+    filename_is_a_link = (filename: string) => filename.startsWith('http');
+
+    // Getting Active Markdown File
+    getActiveNoteFile() {
+        return (this.app.workspace.activeLeaf.view as MarkdownView).file;
+    }
+
+    // Image Name and Alt Text
+    getFileNameAndAltText(linkType: number, match: any){
+        /* linkType 1: [[myimage.jpg|#x-small]]
+           linkType2: ![#x-small](myimage.jpg) 
+        returns { fileName: '', altText: '' }   */
+
+        var file_name_regex;
+        var alt_regex;
+
+        if(linkType == 1){
+            file_name_regex = /(?<=\[\[).*(jpe?g|png|gif)/;
+            alt_regex = /(?<=\|).*(?=]])/;
+        } else if(linkType == 2){
+            file_name_regex = /(?<=\().*(jpe?g|png|gif)/;
+            alt_regex = /(?<=\[)(^$|.*)(?=\])/;
+        }
+
+        var file_match = match[0].match(file_name_regex);
+        var alt_match = match[0].match(alt_regex);
+
+        return { fileName: file_match ? file_match[0] : '', 
+                altText: alt_match ? alt_match[0] : '' }
+
+    }
+
+    // Remove Widgets in CodeMirror Editor
+    clearWidges = (cm: CodeMirror.Editor) => {
+
         var lastLine = cm.lastLine();
 
         for(let i=0; i <= lastLine; i++){
@@ -37,83 +167,16 @@ export default class OzanImagePlugin extends Plugin{
             // Get the current Line
             const line = cm.lineInfo(i);
 
-            // Current Line Comparison with Regex
-            const match_1 = line.text.match(image_line_regex_1);
-            const match_2 = line.text.match(image_line_regex_2);
-
-            // Clear the widget if link was removed
-            var line_image_widget = line.widgets ? line.widgets.filter((wid: { className: string; }) => wid.className === 'oz-image-widget') : false;
-            if(line_image_widget && (!match_1 || !match_2)) line_image_widget[0].clear();
-
-            // If any of regex matches, it will add image widget
-            if(match_1 || match_2){
-
-                // Clear the image widgets if exists
-                if (line.widgets){
-                    for(const wid of line.widgets){
-                        if (wid.className === 'oz-image-widget'){
-                            wid.clear()
-                        }
+            // Clear the image widgets if exists
+            if (line.widgets){
+                for(const wid of line.widgets){
+                    if (wid.className === 'oz-image-widget'){
+                        wid.clear()
                     }
                 }
-
-                // Get the file name and alt text depending on format
-                var filename = '';
-                var alt = '';
-
-                if(match_1){
-                    // Regex for [[ ]] format
-                    var lastChar = match_1[0].length - 2;
-                    filename = match_1[0].substring(3, lastChar);
-                    var first_char = this.getFirstIndexOfImageFileName(filename);
-                    filename = filename.substring(first_char);
-                    alt = 'image';
-                } else if(match_2){
-                    // Regex for ![ ]( ) format
-                    var file_name_regex = /(?<=\().*(jpe?g|png|gif)/;
-                    var alt_regex = /(?<=\[)(^$|.*)(?=\])/
-                    filename = match_2[0].match(file_name_regex)[0];
-                    var first_char = this.getFirstIndexOfImageFileName(filename);
-                    filename = filename.substring(first_char);
-                    alt = match_2[0].match(alt_regex)[0];
-                }
-
-                // Create Image
-                const img = document.createElement('img');
-
-                // Prepare the src for the Image
-                if(this.filename_is_a_link(filename)){
-                    img.src = filename;
-                } else {
-                    let images = files.filter( file => file.path.contains(decodeURIComponent(filename)));
-                    if(images) img.src = this.app.vault.getResourcePath(images[0]);
-                }
-
-                 // Image Properties
-                 img.alt = alt;
-                 img.style.maxWidth = '100%';
-                 img.style.height = 'auto';
-                 
-                 // Add Image widget under the Image Markdown
-                 cm.addLineWidget(i, img, {className: 'oz-image-widget'});
             }
-        }        
+            
+        }
     }
 
-    // Http, Https Link Check
-    filename_is_a_link = (filename: string) => filename.startsWith('http');
-
-    // Index of first char of original image name 
-    getFirstIndexOfImageFileName = (filename: string) => {
-        if(this.filename_is_a_link(filename)){
-            return 0;
-        }
-        if(filename.includes('\\')){
-            return filename.lastIndexOf('\\') + 1;
-        }
-        if(filename.includes('/')){
-            return filename.lastIndexOf('/') + 1;
-        }
-        return 0;
-    }
 }
