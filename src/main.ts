b8e7fd944f517ac62719d78fbf4209d0c399464d
Ozan Tellioglu <ozan@Ozans-MacBook-Pro.local>
2021-04-29 10:09:17 +0200

    Refactoring, changes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { Plugin, Notice, MarkdownView, TFile } from 'obsidian';
+import { Plugin, Notice, TFile } from 'obsidian';
+import { clearWidges, filename_is_a_link, getFileNameAndAltText,
+        getActiveNoteFile, getCmEditor, getPathOfImage } from './utils';
 
 export default class OzanImagePlugin extends Plugin{
 
@@ -7,12 +9,10 @@ export default class OzanImagePlugin extends Plugin{
         this.registerEvent(
             this.app.workspace.on("file-open", this.handleFile)
         )
-
         // Register event for each change
         this.registerCodeMirror( (cm: CodeMirror.Editor) => {
             cm.on("changes", this.codemirrorLineChanges);
         })
-
         // Check the active CodeMirror during load
         this.app.workspace.iterateCodeMirrors( (cm) => {
             this.check_lines(cm);
@@ -33,20 +33,18 @@ export default class OzanImagePlugin extends Plugin{
         this.app.workspace.iterateCodeMirrors( (cm) => {
             this.app.workspace.off("file-open", this.handleFile);
             cm.off("changes", this.codemirrorLineChanges);
-            this.clearWidges(cm);
+            clearWidges(cm);
         });
         new Notice('Image in Editor Plugin is unloaded');
     }
 
     // Check Single Line
-    check_line: any = (cm: CodeMirror.Editor, line_number: number) => {
+    check_line: any = (cm: CodeMirror.Editor, line_number: number, targetFile?:TFile) => {
 
         // Regex for [[ ]] format
         const image_line_regex_1 = /!\[\[.*(jpe?g|png|gif).*\]\]/
-            
         // Regex for ![ ]( ) format
         const image_line_regex_2 = /!\[(^$|.*)\]\(.*(jpe?g|png|gif)\)/
-
         // Get the Line edited
         const line = cm.lineInfo(line_number);
         
@@ -78,25 +76,30 @@ export default class OzanImagePlugin extends Plugin{
             
             if(match_1){
                 // Regex for [[myimage.jpg|#x-small]] format
-                filename = this.getFileNameAndAltText(1, match_1).fileName
-                alt = this.getFileNameAndAltText(1, match_1).altText
+                filename = getFileNameAndAltText(1, match_1).fileName
+                alt = getFileNameAndAltText(1, match_1).altText
             } else if(match_2){
                 // Regex for ![#x-small](myimage.jpg) format
-                filename = this.getFileNameAndAltText(2, match_2).fileName
-                alt = this.getFileNameAndAltText(2, match_2).altText
+                filename = getFileNameAndAltText(2, match_2).fileName
+                alt = getFileNameAndAltText(2, match_2).altText
             }
             
             // Create Image
             const img = document.createElement('img');
 
             // Prepare the src for the Image
-            if(this.filename_is_a_link(filename)){
+            if(filename_is_a_link(filename)){
                 img.src = filename;
             } else {
-                // TODO: this.getActiveNoteFile() to be replaced: if 2-3 notes are open during load, only the active one loads images
-                var image = this.app.metadataCache.getFirstLinkpathDest(decodeURIComponent(filename), this.getActiveNoteFile().path);
-                if(image != null) img.src =  this.app.vault.getResourcePath(image); 
-                // NOTE: doesn't blink with : 'app://local/Users/ozan/Desktop/Debug/' + image.path
+                // Source Path
+                var sourcePath = '';
+                if(targetFile){
+                    sourcePath = targetFile.path;
+                }else{
+                    sourcePath = getActiveNoteFile(this.app.workspace).path;
+                }
+                var image = this.app.metadataCache.getFirstLinkpathDest(decodeURIComponent(filename), sourcePath);
+                if(image != null) img.src = getPathOfImage(this.app.vault, image)
             }
 
             // Image Properties
@@ -110,81 +113,24 @@ export default class OzanImagePlugin extends Plugin{
     }
 
     // Check All Lines Function
-    check_lines: any = (cm: CodeMirror.Editor) => {
+    check_lines: any = (cm: CodeMirror.Editor, targetFile?:TFile) => {
         // Last Used Line Number in Code Mirror
         var lastLine = cm.lastLine();
         for(let i=0; i <= lastLine; i++){
-            this.check_line(cm, i);
+            if(TFile){
+                this.check_line(cm, i)
+            }else{
+                this.check_line(cm, i, TFile);
+            }
         }        
     }
 
-    // Http, Https Link Check
-    filename_is_a_link = (filename: string) => filename.startsWith('http');
-
-    // Getting Active Markdown File
-    getActiveNoteFile() {
-        return (this.app.workspace.activeLeaf.view as MarkdownView).file;
-    }
-
-    // Image Name and Alt Text
-    getFileNameAndAltText(linkType: number, match: any){
-        /* linkType 1: [[myimage.jpg|#x-small]]
-           linkType2: ![#x-small](myimage.jpg) 
-        returns { fileName: '', altText: '' }   */
-        var file_name_regex;
-        var alt_regex;
-
-        if(linkType == 1){
-            file_name_regex = /(?<=\[\[).*(jpe?g|png|gif)/;
-            alt_regex = /(?<=\|).*(?=]])/;
-        } else if(linkType == 2){
-            file_name_regex = /(?<=\().*(jpe?g|png|gif)/;
-            alt_regex = /(?<=\[)(^$|.*)(?=\])/;
-        }
-
-        var file_match = match[0].match(file_name_regex);
-        var alt_match = match[0].match(alt_regex);
-
-        return { fileName: file_match ? file_match[0] : '', 
-                altText: alt_match ? alt_match[0] : '' }
-
-    }
-
-    // Remove Widgets in CodeMirror Editor
-    clearWidges = (cm: CodeMirror.Editor) => {
-        var lastLine = cm.lastLine();
-        for(let i=0; i <= lastLine; i++){
-            // Get the current Line
-            const line = cm.lineInfo(i);
-            // Clear the image widgets if exists
-            if (line.widgets){
-                for(const wid of line.widgets){
-                    if (wid.className === 'oz-image-widget'){
-                        wid.clear()
-                    }
-                }
-            }
-            
-        }
-    }
-
-    // Get Active Editor
-    getEditor = () => {
-        return this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
-    }
-
     // Handle file after file-open event
-    handleFile = (file: TFile): void => {
+    handleFile = (targetFile: TFile): void => {
         // If the file fired is a markdown file
-        if(file.extension === 'md'){
+        if(targetFile.extension === 'md'){
             // Get the open CodeMirror to check the lines
-            this.app.workspace.iterateCodeMirrors( (cm) => {
-                // TODO: There is no Public API to read cm from Editor
-                var editor = this.getEditor();
-                if(editor.cm == cm){
-                    this.check_lines(cm);
-                }
-            })
-        }  
+            this.check_lines(getCmEditor(this.app.workspace), targetFile);
+        }
     }
 }
---
+++ src/utils.ts
@@ -0,0 +1,77 @@
+import { Workspace, MarkdownView, Vault, TFile, normalizePath } from 'obsidian';
+
+// Remove Widgets in CodeMirror Editor
+const clearWidges = (cm: CodeMirror.Editor) => {
+
+    var lastLine = cm.lastLine();
+
+    for(let i=0; i <= lastLine; i++){
+
+        // Get the current Line
+        const line = cm.lineInfo(i);
+
+        // Clear the image widgets if exists
+        if (line.widgets){
+            for(const wid of line.widgets){
+                if (wid.className === 'oz-image-widget'){
+                    wid.clear()
+                }
+            }
+        }
+        
+    }
+}
+
+// Http, Https Link Check
+const filename_is_a_link = (filename: string) => filename.startsWith('http');
+
+ // Image Name and Alt Text
+const getFileNameAndAltText =(linkType: number, match: any) => {
+
+    /* 
+       linkType 1: [[myimage.jpg|#x-small]]
+       linkType2: ![#x-small](myimage.jpg) 
+       returns { fileName: '', altText: '' }   
+    */
+
+    var file_name_regex;
+    var alt_regex;
+
+    if(linkType == 1){
+        file_name_regex = /(?<=\[\[).*(jpe?g|png|gif)/;
+        alt_regex = /(?<=\|).*(?=]])/;
+    } else if(linkType == 2){
+        file_name_regex = /(?<=\().*(jpe?g|png|gif)/;
+        alt_regex = /(?<=\[)(^$|.*)(?=\])/;
+    }
+
+    var file_match = match[0].match(file_name_regex);
+    var alt_match = match[0].match(alt_regex);
+
+    return { fileName: file_match ? file_match[0] : '', 
+            altText: alt_match ? alt_match[0] : '' }
+
+}    
+
+// Getting Active Markdown File
+const getActiveNoteFile = (workspace: Workspace) => {
+    return (workspace.activeLeaf.view as MarkdownView).file;
+}
+
+// Get Active Editor
+const getCmEditor = (workspace: Workspace): CodeMirror.Editor => {
+    return workspace.getActiveViewOfType(MarkdownView)?.sourceMode?.cmEditor
+}
+
+const getPathOfVault = (vault: Vault): string => {
+    return 'app://local' + vault.adapter.basePath
+}
+
+// Temporary Solution until getResourcePath improved 
+const getPathOfImage = (vault: Vault, image: TFile) => {
+    // this.app.vault.getResourcePath(image) 
+    return getPathOfVault(vault) + '/' + image.path
+}
+
+export { clearWidges, filename_is_a_link, getFileNameAndAltText,
+    getActiveNoteFile, getCmEditor, getPathOfImage };
