5be202e89501398ed71511d14c51612f0a304df2
Ozan Tellioglu <ozntel@gmail.com>
2021-08-30 08:34:16 +0200

    Settings Link

--- src/settings.ts
+++ src/settings.ts
@@ -112,5 +112,15 @@ export class OzanImagePluginSettingsTab extends PluginSettingTab {
 					this.plugin.saveSettings();
 				})
 			);
+
+		const coffeeDiv = containerEl.createDiv('coffee');
+		coffeeDiv.addClass('oz-coffee-div');
+		const coffeeLink = coffeeDiv.createEl('a', { href: 'https://ko-fi.com/L3L356V6Q' });
+		const coffeeImg = coffeeLink.createEl('img', {
+			attr: {
+				src: 'https://cdn.ko-fi.com/cdn/kofi2.png?v=3',
+			},
+		});
+		coffeeImg.height = 45;
 	}
 }
