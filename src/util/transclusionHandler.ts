440e57bce7bce095e20ef7055a3d4b4fbc14c3e6
Ozan Tellioglu <ozntel@gmail.com>
2021-09-07 15:40:08 +0200

    Admonition Colors

--- src/util/transclusionHandler.ts
+++ src/util/transclusionHandler.ts
@@ -185,8 +185,9 @@ const convertAdmonitions = (html: HTMLElement) => {
 
 const createAdmonitionEl = (title: string, content: string): HTMLElement => {
 	let adEl = document.createElement('div');
+	let colors = admonitionColorMap[title] ? admonitionColorMap[title] : '68, 138, 255';
 	adEl.innerHTML = `
-    <div class="admonition admonition-plugin" style="--admonition-color: 68, 138, 255;">
+    <div class="admonition admonition-plugin" style="--admonition-color: ${colors};">
         <div class="admonition-title">
             <div class="admonition-title-content">
                 <div class="admonition-title-markdown" id="oz-admonition-title">
@@ -207,4 +208,33 @@ const createAdmonitionEl = (title: string, content: string): HTMLElement => {
 const admonitionPluginActive = (app: App) => {
 	// @ts-ignore
 	return app.plugins.getPlugin('obsidian-admonition');
+};
+
+const admonitionColorMap: { [key: string]: string } = {
+	abstract: '0, 176, 255',
+	attention: '255, 145, 0',
+	bug: '245, 0, 87',
+	caution: '255, 145, 0',
+	check: '0, 200, 83',
+	cite: '158, 158, 158',
+	danger: '255, 23, 68',
+	done: '0, 200, 83',
+	error: '255, 23, 68',
+	example: '124, 77, 255',
+	fail: '255, 82, 82',
+	failure: '255, 82, 82',
+	faq: '100, 221, 23',
+	help: '100, 221, 23',
+	hint: '0, 191, 165',
+	important: '0, 191, 165',
+	info: '0, 184, 212',
+	missing: '255, 82, 82',
+	note: '68, 138, 255',
+	question: '100, 221, 23',
+	quote: '158, 158, 158',
+	seealso: '68, 138, 255',
+	success: '0, 200, 83',
+	summary: '0, 176, 255',
+	tip: '0, 191, 165',
+	todo: '0, 184, 212',
 };
