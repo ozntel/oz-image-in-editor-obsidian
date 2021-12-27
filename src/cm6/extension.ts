d54bf614162b159d690169a4450af2dc6182f0c1
Ozan Tellioglu <ozntel@gmail.com>
2021-12-27 10:20:28 +0100

    Decoration Builder Change

--- src/cm6/extension.ts
+++ src/cm6/extension.ts
@@ -11,8 +11,8 @@ import * as CM6Helpers from 'src/cm6/cm6Helper';
 export const images = (params: { plugin: OzanImagePlugin }): Extension => {
     const { plugin } = params;
 
-    const decorate = (params: { state: EditorState; newDoc: Text }) => {
-        const { newDoc, state } = params;
+    const getDecorationsForLines = (params: { lineNrs: number[]; state: EditorState; newDoc: Text }) => {
+        const { newDoc, state, lineNrs } = params;
 
         let rangeBuilder = new RangeSetBuilder<Decoration>();
 
@@ -20,45 +20,37 @@ export const images = (params: { plugin: OzanImagePlugin }): Extension => {
         const mdView = state.field(editorViewField);
         const sourceFile: TFile = mdView.file;
 
-        // --> Check Lines Having Links or HTML Elements
-        let lineNrs = CM6Helpers.getLinesToCheckForRender(state, newDoc);
-
         // --> Loop Through Lines Found
         if (lineNrs.length > 0) {
             for (const lineNr of lineNrs) {
                 const line = newDoc.line(lineNr);
+                let newDeco: Decoration = null;
 
                 // --> Look at Link Result
                 const linkResult = detectLink({ lineText: line.text, plugin: plugin, sourceFile: sourceFile });
 
                 // --> External Link Render
                 if (linkResult && linkResult.type === 'external-image') {
-                    rangeBuilder.add(
-                        line.to,
-                        line.to,
-                        ImageDecoration({ url: linkResult.linkText, altText: linkResult.altText, filePath: linkResult.linkText })
-                    );
+                    newDeco = ImageDecoration({ url: linkResult.linkText, altText: linkResult.altText, filePath: linkResult.linkText });
                 }
 
                 // --> Vault Image Render
                 else if (linkResult && linkResult.type === 'vault-image') {
                     let imagePath = ObsidianHelpers.getPathOfImage(plugin.app.vault, linkResult.file);
-                    rangeBuilder.add(line.to, line.to, ImageDecoration({ url: imagePath, altText: linkResult.altText, filePath: linkResult.file.path }));
+                    newDeco = ImageDecoration({ url: imagePath, altText: linkResult.altText, filePath: linkResult.file.path });
                 }
 
                 // --> External PDF Link Render
                 else if (linkResult && linkResult.type === 'pdf-link') {
-                    rangeBuilder.add(
-                        line.to,
-                        line.to,
-                        PDFDecoration({ url: linkResult.linkText + linkResult.blockRef, filePath: linkResult.linkText + linkResult.blockRef })
-                    );
+                    newDeco = PDFDecoration({ url: linkResult.linkText + linkResult.blockRef, filePath: linkResult.linkText + linkResult.blockRef });
                 }
 
                 // --> Iframe Render
                 else if (linkResult && linkResult.type === 'iframe') {
-                    rangeBuilder.add(line.to, line.to, CustomHTMLDecoration({ htmlText: linkResult.match }));
+                    newDeco = CustomHTMLDecoration({ htmlText: linkResult.match });
                 }
+
+                if (newDeco !== null) rangeBuilder.add(line.to, line.to, newDeco);
             }
         }
 
@@ -68,7 +60,8 @@ export const images = (params: { plugin: OzanImagePlugin }): Extension => {
     const imagesField = StateField.define<DecorationSet>({
         create: (state: EditorState) => {
             if (!ObsidianHelpers.livePreviewActive(plugin.app)) {
-                return decorate({ state, newDoc: state.doc });
+                let lineNrs = CM6Helpers.getLinesToCheckForRender(state, state.doc);
+                return getDecorationsForLines({ lineNrs, state, newDoc: state.doc });
             } else {
                 return Decoration.none;
             }
@@ -76,7 +69,10 @@ export const images = (params: { plugin: OzanImagePlugin }): Extension => {
 
         update: (effects: DecorationSet, transaction: Transaction) => {
             if (transaction.docChanged && !ObsidianHelpers.livePreviewActive(plugin.app)) {
-                return decorate({ state: transaction.state, newDoc: transaction.newDoc });
+                const state = transaction.state;
+                const newDoc = transaction.newDoc;
+                let lineNrs = CM6Helpers.getLinesToCheckForRender(state, newDoc);
+                return getDecorationsForLines({ lineNrs, state, newDoc });
             }
             return effects.map(transaction.changes);
         },
