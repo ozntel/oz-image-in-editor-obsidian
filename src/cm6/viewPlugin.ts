33a81ddb6da6400536ef0b494d4408980bf2d6d6
Ozan Tellioglu <ozntel@gmail.com>
2022-01-12 12:27:38 +0100

    Blanking Issue Solution

--- src/cm6/viewPlugin.ts
+++ src/cm6/viewPlugin.ts
@@ -16,14 +16,14 @@ export const getViewPlugin = (params: { plugin: OzanImagePlugin }): Extension =>
                 this.decoManager = new StatefulDecorationSet(view);
                 if (!livePreviewActive(plugin.app)) {
                     const state = view.state;
-                    this.decoManager.debouncedUpdate({ view, state, newDoc: state.doc, plugin });
+                    this.decoManager.updateAsyncDecorations({ view, state, newDoc: state.doc, plugin });
                 }
             }
 
             update(update: ViewUpdate) {
                 if ((update.docChanged || update.viewportChanged) && !livePreviewActive(plugin.app)) {
                     const state = update.view.state;
-                    this.decoManager.debouncedUpdate({ view: update.view, plugin, newDoc: state.doc });
+                    this.decoManager.updateAsyncDecorations({ view: update.view, plugin, newDoc: state.doc });
                 }
             }
 
