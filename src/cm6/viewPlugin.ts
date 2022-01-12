667351d6f4b7aa25aea14c5c7ef66e48d79c6f78
Ozan Tellioglu <ozntel@gmail.com>
2022-01-12 02:13:13 +0100

    Live Preview Check Update

--- src/cm6/viewPlugin.ts
+++ src/cm6/viewPlugin.ts
@@ -2,6 +2,7 @@ import { Extension } from '@codemirror/state';
 import { EditorView, ViewPlugin, ViewUpdate } from '@codemirror/view';
 import OzanImagePlugin from 'src/main';
 import { StatefulDecorationSet } from 'src/cm6/decorations';
+import { livePreviewActive } from 'src/util/obsidianHelper';
 
 // --> View Plugin
 export const getViewPlugin = (params: { plugin: OzanImagePlugin }): Extension => {
@@ -13,12 +14,14 @@ export const getViewPlugin = (params: { plugin: OzanImagePlugin }): Extension =>
 
             constructor(view: EditorView) {
                 this.decoManager = new StatefulDecorationSet(view);
-                const state = view.state;
-                this.decoManager.debouncedUpdate({ view, state, newDoc: state.doc, plugin });
+                if (!livePreviewActive(plugin.app)) {
+                    const state = view.state;
+                    this.decoManager.debouncedUpdate({ view, state, newDoc: state.doc, plugin });
+                }
             }
 
             update(update: ViewUpdate) {
-                if (update.docChanged || update.viewportChanged) {
+                if ((update.docChanged || update.viewportChanged) && !livePreviewActive(plugin.app)) {
                     const state = update.view.state;
                     this.decoManager.debouncedUpdate({ view: update.view, plugin, newDoc: state.doc });
                 }
